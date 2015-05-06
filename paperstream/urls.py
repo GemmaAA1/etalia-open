d0084629c7a217ba28b17c35fac252f21c031798
NPann <nicolas.pannetier@gmail.com>
2015-05-06 10:55:21 -0700

    update url patterns

--- paperstream/urls.py
+++ paperstream/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from base import views as base_views
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'paperstream.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', base_views.home, name='home'),
+    url(r'^library/', include('library.urls')),
+    # url(r'^accounts/', include('accounts.urls')),
+    # url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
