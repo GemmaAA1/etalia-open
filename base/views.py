ebf267a9ba41b1a813f9a6a063474d89ac46318a
NPann <nicolas.pannetier@gmail.com>
2015-05-09 17:00:38 -0700

    change project architecture

--- base/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-
-
-def home(request):
-    # TODO: remove this and make url in template dynamic
-    host = request.get_host()
-    return render(request, 'landing.html', {'host': host})
