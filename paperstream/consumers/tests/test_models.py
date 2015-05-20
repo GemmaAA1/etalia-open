0f19513d6ca0573a78d875e19df78a4e756ebe04
NPann <nicolas.pannetier@gmail.com>
2015-05-20 16:28:54 -0700

    work on test for parsers and models

--- paperstream/consumers/tests/test_models.py
+++ paperstream/consumers/tests/test_models.py
@@ -1,7 +1,10 @@
+import json
+from unittest.mock import patch
 from django.test import TestCase
-from ..models import ConsumerPubmed, ConsumerJournal
-from library.models import Journal, Paper
 from django.core.exceptions import ValidationError
+from django.utils import timezone
+from library.models import Journal, Paper
+from ..models import ConsumerPubmed, ConsumerJournal
 
 
 class ConsumerTest(TestCase):
@@ -69,14 +72,13 @@ class ConsumerPubmedTest(TestCase):
         journal, _ = Journal.objects.get_or_create(id_issn='1053-8119')
         consumer, _ = ConsumerPubmed.objects.get_or_create(name='pub cons #1')
         journal = Journal.objects.first()
+        consumer.day0 = 10
         consumer.add_journal(journal)
+        consumer.activate_journal(journal)
+        consumer.save()
 
-    def test_consume_journal_update_consumerjournal(self):
-        journal = Journal.objects.first()
-        consumer = ConsumerPubmed.objects.first()
-        consumer.consume_journal(journal)
-        self.assertIsNotNone(consumer.consumerjournal_set.first().last_date_cons)
-        self.assertTrue(consumer.consumerjournal_set.first().last_number_papers > 0)
+        with open('consumers/tests/pubmed_sample.json') as file:
+            self.entries = json.load(file)
 
     def test_consume_journal_populate_stats(self):
         journal = Journal.objects.first()
@@ -87,14 +89,31 @@ class ConsumerPubmedTest(TestCase):
         cjs = cj.stats.all()
         self.assertEqual(cjs.count(), 2)
 
-    def test_consumer_populate_journal(self):
+    @patch('consumers.models.ConsumerPubmed.get_q')
+    def test_consumer_populate_journal(self, mock_get_q):
+        # setup mock object
+        mock_get_q.return_value = self.entries
+
         journal = Journal.objects.first()
         consumer = ConsumerPubmed.objects.first()
-        consumer.day0 = 10
-        consumer.add_journal(journal)
-        consumer.activate_journal(journal)
+
         papers = Paper.objects.filter(journal=journal)
         self.assertTrue(papers.count() == 0)
         consumer.populate_journal(journal)
         papers = Paper.objects.filter(journal=journal)
-        self.assertTrue(papers.count() > 0)
+        self.assertEqual(papers.count(), 10)
+
+    @patch('consumers.models.ConsumerPubmed.get_q')
+    def test_consumer_update_journal(self, mock_get_q):
+        # setup mock object
+        mock_get_q.return_value = self.entries
+
+        journal = Journal.objects.first()
+        consumer = ConsumerPubmed.objects.first()
+        consumer.populate_journal(journal)
+
+        self.assertIsNotNone(consumer.consumerjournal_set.first().last_date_cons)
+        self.assertTrue(consumer.consumerjournal_set.first().
+                        last_number_papers_retrieved > 0)
+        self.assertTrue(consumer.consumerjournal_set.first().
+                        last_number_papers_fetched > 0)
---
+++ paperstream/consumers/tests/test_parsers.py
@@ -0,0 +1,97 @@
+import json
+from unittest.mock import patch
+from django.test import TestCase
+from dateutil.parser import parse
+from ..parsers import ParserPubmed, ParserArxiv, ParserElsevier
+
+# Not sure what the best way to unit test parser.
+# If test is ran on multiple entries, testing code might really
+# look like parser code if we do not manually enter all the entry fields
+
+
+class ParserPubmedTest(TestCase):
+
+    def setUp(self):
+        with open('consumers/tests/pubmed_sample.json') as file:
+            self.entries = json.load(file)
+
+    def test_parse_journal(self):
+        # test only first record
+        entry = self.entries[0]
+        parser = ParserPubmed()
+        journal = parser.parse_journal(entry)
+        self.assertEqual(journal['title'], 'Neurosurgery')
+        self.assertEqual(journal['id_issn'], '0148-396X')
+        self.assertEqual(journal['id_eissn'], '1524-4040')
+
+    def test_parse_paper(self):
+        # test only first record
+        entry = self.entries[0]
+        parser = ParserPubmed()
+        paper = parser.parse_paper(entry)
+        self.assertEqual(paper['id_doi'], '10.1227/NEU.0000000000000714')
+        self.assertEqual(paper['id_pii'], '00006123-201506000-00024')
+        self.assertEqual(paper['abstract'][:len('BACKGROUND:')], 'BACKGROUND:')
+        self.assertEqual(paper['type'], 'JOUR')
+        self.assertEqual(paper['id_pmi'], '25988929')
+        self.assertEqual(paper['publish_status'], 'ppublish')
+        self.assertEqual(paper['volume'], '76')
+        self.assertEqual(paper['issue'], '6')
+        self.assertEqual(paper['page'], '756-765')
+        self.assertEqual(paper['date_ep'], None)
+        self.assertEqual(paper['date_pp'], parse('2015 Jun'))
+        self.assertEqual(paper['date_lr'], parse('20150520'))
+        self.assertEqual(paper['url'], 'http://doi.org/10.1227/NEU.0000000000000714')
+        self.assertEqual(paper['language'], 'ENG')
+        self.assertEqual(paper['source'], '')
+
+    def test_parse_authors(self):
+        entry = self.entries[0]
+        parser = ParserPubmed()
+        authors = parser.parse_authors(entry)
+        self.assertEqual(authors[0]['first_name'], 'Srivatsan')
+        self.assertEqual(authors[0]['last_name'], 'Pallavaram')
+        self.assertEqual(authors[1]['first_name'], 'Pierre-Francois')
+        self.assertEqual(authors[1]['last_name'], "D'Haese")
+        self.assertEqual(authors[-1]['first_name'], 'Joseph S')
+        self.assertEqual(authors[-1]['last_name'], 'Neimat')
+
+    def test_parse_corp_author(self):
+        pass
+
+    def test_parse_all(self):
+        records = []
+        parser = ParserPubmed()
+        for entry in self.entries:
+            records.append(parser.parse(entry))
+        self.assertEqual(len(records), len(self.entries))
+
+
+class ParserElsevierTest(TestCase):
+
+    def test_parserelsevier_journal(self):
+        pass
+
+    def test_parserelsevier_paper(self):
+        pass
+
+    def test_parserelsevier_authors(self):
+        pass
+
+    def test_parserelsevier_corp_author(self):
+        pass
+
+
+class ParserArxivTest(TestCase):
+
+    def test_parserarxiv_journal(self):
+        pass
+
+    def test_parserarxiv_paper(self):
+        pass
+
+    def test_parserarxiv_authors(self):
+        pass
+
+    def test_parserarxiv_corp_author(self):
+        pass
