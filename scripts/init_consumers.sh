cc75ad260a87ea0aae17b80a43e4cc5dfdc29e8e
NPann <nicolas.pannetier@gmail.com>
2015-06-17 22:38:00 -0700

    add init script to populate library and consumers

--- scripts/init_consumers.sh
+++ scripts/init_consumers.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#./manage.py populate publisher all
-#./manage.py populate journal all
 ./manage.py populate consumer pubmed --name pubmed_all
 ./manage.py populate consumer arxiv --name arxiv_all
 ./manage.py populate consumer elsevier --name elsevier_all
---
+++ scripts/init_library.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./manage.py populate publisher all
+./manage.py populate journal thomson
+./manage.py populate journal pubmed
+./manage.py populate journal arxiv
