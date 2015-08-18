11238d5c88db4146e3336b4f9268b6917717a3b6
NPann <nicolas.pannetier@gmail.com>
2015-08-18 09:16:44 -0700

    add mixin search

--- paperstream/core/views.py
+++ paperstream/core/views.py
@@ -9,3 +9,17 @@ def home(request):
 
 def test(request):
     return render(request, 'test.html', {})
+
+
+class TitleSearchMixin(object):
+
+    def get_queryset(self):
+        # Fetch the queryset from the parent's get_queryset
+        queryset = super(TitleSearchMixin, self).get_queryset()
+        # Get the q GET parameter
+        q = self.request.GET.get("q")
+        if q:
+            # return a filtered queryset
+            return queryset.filter(title__icontains=q)
+        # No q is specified so we return queryset
+        return queryset
