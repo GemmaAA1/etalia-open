224df83d646c7d5a214eb20ac5a3e616c63d9473
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-08-28 13:44:57 -0700

    update heroku settings

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -5,7 +5,9 @@ from .common import *
 
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = ["*"]
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['localhost', 'paperstream.herokuapp.com']
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
@@ -13,9 +15,6 @@ DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 STATIC_ROOT = 'staticfiles'
