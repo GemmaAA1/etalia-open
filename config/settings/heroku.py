4eb63b7f3107d088eb5433d7cf75553131598679
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-08-28 01:50:21 -0700

    add heroku files

---
+++ config/settings/heroku.py
@@ -0,0 +1,38 @@
+from .common import *
+
+DEBUG = env.bool('DJANGO_DEBUG', default=False)
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+# # Static asset cocnfiguration
+# STATIC_ROOT = 'staticfiles'
+# STATICFILES_DIRS = []
+#
+# # Amazon
+# # S3
+# INSTALLED_APPS += ('storages',)
+# AWS_STORAGE_BUCKET_NAME = "paperstreamstatic"
+# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+# STATIC_URL = S3_URL
+
