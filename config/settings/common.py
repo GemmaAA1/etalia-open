7b453ca7a78e07a93e489dc1ea66659be56b9a61
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-08-28 15:35:00 -0700

    formating

--- config/settings/common.py
+++ config/settings/common.py
@@ -93,10 +93,6 @@ DATABASES = {
         # pipeline is atomic while celery needs to know user during the pipeline
         # authentication process TODO: find a fix ?
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(ROOT_DIR, '../database/db.sqlite3'),
-    # }
 }
 
 # TEMPLATE CONFIGURATION
