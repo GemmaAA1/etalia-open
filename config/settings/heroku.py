3d42acd5512988d8e1cd1a3f758917835bdcc083
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-08-28 17:02:39 -0700

    update allow hosts

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -2,7 +2,8 @@ from .heroku_local import *
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['paperstream.herokuapp.com']
+ALLOWED_HOSTS = ['shrouded-peak-3632.herokuapp.com',
+                 'shrouded-peak.paperstream.io']
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
