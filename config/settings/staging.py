5c6ca4c7eb9dc4d28465c381a7ce8fe2d534ce16
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-09-02 15:44:28 -0700

    update database setup

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -8,10 +8,10 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env.str('RDS_DB_NAME'),
-        'USER': env.str('RDS_USERNAME'),
-        'PASSWORD': env.str('RDS_PASSWORD'),
-        'HOST': env.str('RDS_HOSTNAME'),
-        'PORT': env.str('RDS_PORT'),
+        'NAME': env.str('AWS_RDS_DB_NAME'),
+        'USER': env.str('AWS_RDS_USERNAME'),
+        'PASSWORD': env.str('AWS_RDS_PASSWORD'),
+        'HOST': env.str('AWS_RDS_HOSTNAME'),
+        'PORT': env.str('AWS_RDS_PORT'),
     }
 }
