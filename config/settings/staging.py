c983a58fb924a3e8e7edefe217996d0de604485d
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-09-03 14:47:44 -0700

    update staging ALLOWED_HOSTS

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -3,7 +3,7 @@ from .common import *
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['staging-stack.paperstream.io']
 
 DATABASES = {
     'default': {
