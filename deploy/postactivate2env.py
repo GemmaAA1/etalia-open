e4877771460c6034318b4320c309fc4d8f4dc666
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-09-04 17:32:50 -0700

    push postactivate2env.py

---
+++ deploy/postactivate2env.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+"""To grab env var from postactivate"""
+
+import sys
+import getopt
+import re
+import subprocess
+
+def main(argv):
+    inputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:",["ifile="])
+    except getopt.GetoptError:
+        print 'test.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'postactivate2env.py -i <inputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+
+    if inputfile:
+        vars = ''
+        with open(inputfile, 'r') as f:
+            for line in f:
+                if line.strip().startswith('export'):
+
+                    line = re.sub('export ', '', line.strip()).strip()
+                    # escape % character
+                    line = re.sub('%', '%%', line)
+                    if vars:  # add a comma
+                        vars += ','
+                    vars += line
+        # store in environment variable
+        subprocess.call(["export ENV_FOR_SUPERVISOR=", '"'+vars+'"'], shell=True)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
