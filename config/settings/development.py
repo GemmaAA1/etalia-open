3a3e153d1babff6e6f4d2dc375df73ced753c6d5
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-09-12 17:36:11 -0700

    update templates modal

--- config/settings/development.py
+++ config/settings/development.py
@@ -42,5 +42,5 @@ NLP_LSH_BUCKET_NAME = 'paperstream-development-nlp-lshs'
 # ------------------------------------------------------------------------------
 # In development, all tasks will be executed locally by blocking until the task
 # returns
-CELERY_ALWAYS_EAGER = True
+# CELERY_ALWAYS_EAGER = True
 
