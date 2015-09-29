f40214ef5661e677dad645e656e8fce788858549
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-09-29 00:11:08 -0700

    update fabfile and add migrations

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -58,7 +58,8 @@ INSTANCE_TYPES_RANK = { 't2.micro': 0,
                         't2.large': 3,
                         'm4.large': 3,
                         'm4.xlarge': 4}
-ROLE_INSTANCE_TYPE_MAP = {'base': 't2.small',
+ROLE_INSTANCE_TYPE_MAP = {'web': 't2.micro',
+                          'base': 't2.small',
                           'lsh': 't2.medium',
                           'nlp': 't2.large',
                           'spot': 'm4.large'}
