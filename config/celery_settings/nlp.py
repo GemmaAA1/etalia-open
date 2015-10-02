cfc5f9ab21906440cc0405bedd42257d45f162e1
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-10-02 15:10:04 -0700

    update celery broker nlp

--- config/celery_settings/nlp.py
+++ config/celery_settings/nlp.py
@@ -10,8 +10,11 @@ APPS_DIR = ROOT_DIR.path('paperstream')
 
 env = environ.Env()
 
-
-BROKER_URL = 'amqp://'
+BROKER_URL = 'amqp://{username}:{password}@{host}:5672//'.format(
+    username=env.str('RABBITMQ_USERNAME'),
+    password=env.str('RABBITMQ_PASSWORD'),
+    host=env.str('RABBITMQ_HOSTNAME'),
+)
 CELERY_RESULT_BACKEND = 'amqp://'
 CELERY_ACCEPT_CONTENT = ['json', 'pickle']
 CELERY_TASK_RESULT_EXPIRES = 5  # in seconds
