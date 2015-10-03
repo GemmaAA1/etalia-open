6daade757f87ced49bd687e4f5717ea2e640fea3
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-10-02 18:49:27 -0700

    ...

--- config/celery_settings/staging/base.py
+++ config/celery_settings/staging/base.py
@@ -9,4 +9,8 @@ BROKER_URL = 'amqp://{username}:{password}@{host}:5672//'.format(
     password=env.str('RABBITMQ_PASSWORD'),
     host=env.str('RABBITMQ_HOSTNAME'),
 )
-CELERY_RESULT_BACKEND = 'amqp://'
+CELERY_RESULT_BACKEND = 'amqp://{username}:{password}@{host}:5672//'.format(
+    username=env.str('RABBITMQ_USERNAME'),
+    password=env.str('RABBITMQ_PASSWORD'),
+    host=env.str('RABBITMQ_HOSTNAME'),
+)
