1226c83aae64411871c4ec7b777b880f3e8a3fe4
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-10-02 21:47:49 -0700

    update base celery

--- config/celery_settings/staging/base.py
+++ config/celery_settings/staging/base.py
@@ -14,9 +14,3 @@ CELERY_RESULT_BACKEND = 'amqp://{username}:{password}@{host}:5672//'.format(
     password=env.str('RABBITMQ_PASSWORD'),
     host=env.str('RABBITMQ_HOSTNAME'),
 )
-
-CELERY_IGNORE_RESULT = True
-
-CELERY_ANNOTATIONS = {'paperstream.nlp.tasks.dbow-128-with-words': {'rate_limit': '200/s'},
-                      'paperstream.nlp.tasks.dbow-128': {'rate_limit': '200/s'}}
-
