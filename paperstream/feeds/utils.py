4b4e0cc4587203c7281a94bd2ed6e3a1225b760c
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2015-12-16 13:47:06 -0800

    update content based scoring

--- paperstream/feeds/utils.py
+++ paperstream/feeds/utils.py
@@ -33,8 +33,8 @@ class StreamScoring(object):
         self.seed_auth_data = []
         self.target_auth_data = []
         self.profile = []
-        self.vec_w = 1.
-        self.auth_w = 0.
+        self.vec_w = 0.
+        self.auth_w = 1.
         self.jour_w = 0.
         # contains all [journal_pk]:vectors for journals from matches in seed_pks
         self._journal_dict = {}
