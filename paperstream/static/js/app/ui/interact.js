9970c764fa1c6d4e88821416d9bb83e75499e611
Etienne Dauvergne <contact@ekyna.com>
2016-02-21 10:02:21 +0100

    * Issue #18 main nav counters css. * Issue #17 fix ga events.

--- paperstream/static/js/app/ui/interact.js
+++ paperstream/static/js/app/ui/interact.js
@@ -1,70 +1,103 @@
 define(['jquery'], function($) {
 
-    var $body = $('body'),
-        category = 'Control';
 
-    function initHandlers() {
+    var Interact = function(options) {
+        this.config = $.extend({
+            debug: false
+        }, options);
+    };
+
+    Interact.prototype.log = function() {
+        if (this.config.debug) {
+            console.log('[Interact] ' + arguments[0], Array.prototype.splice.call(arguments, 1));
+        }
+
+        return this;
+    };
+
+    Interact.prototype.init = function() {
+        this.log('init()');
+        var that = this,
+            count = 0,
+            gaInterval = setInterval(function() {
+                that.log('check ga', count, typeof ga);
+                if (typeof ga == "function") {
+                    that.initHandlers();
+                    clearInterval(gaInterval);
+                    return;
+                }
+                count++;
+                if (count > 20) { // Stop check after 10 seconds
+                    clearInterval(gaInterval);
+                }
+            }, 500);
+
+        return this;
+    };
+
+    Interact.prototype.initHandlers = function() {
+        this.log('initHandlers()');
+
+        var that = this,
+            $body = $('body'),
+            category = 'control';
+
         $body
             .on('etalia.control.search.change', function(e, data) {
-                ga('send', {
+                var obj = {
                     hitType:       'event',
                     eventCategory: category,
                     eventAction:   'search',
-                    eventLabel:    '',
-                    eventValue:    ''
-                });
+                    eventLabel:    data.expression
+                };
+                that.log('ga.send()', obj);
+                ga('send', obj);
             })
             .on('etalia.control.timespan.change', function(e, data) {
-                ga('send', {
+                var obj = {
                     hitType:       'event',
                     eventCategory: category,
                     eventAction:   'timespan',
                     eventLabel:    data.label,
                     eventValue:    data.value
-                });
+                };
+                that.log('ga.send()', obj);
+                ga('send', obj);
             })
             .on('etalia.control.cluster.change', function(e, data) {
-                ga('send', {
+                var obj = {
                     hitType:       'event',
                     eventCategory: category,
                     eventAction:   'cluster',
                     eventLabel:    data.label,
                     eventValue:    data.value
-                });
+                };
+                that.log('ga.send()', obj);
+                ga('send', obj);
             })
             .on('etalia.control.pinned.change', function(e, data) {
-                ga('send', {
+                var obj = {
                     hitType:       'event',
                     eventCategory: category,
-                    eventAction:   'pin',
-                    eventLabel:    data.active ? 'on' : 'off',
-                    eventValue:    data.active ? 1 : 0
-                });
+                    eventAction:   data.active ? 'pin' : 'unpin'
+                };
+                that.log('ga.send()', obj);
+                ga('send', obj);
             })
             .on('etalia.control.filters.change', function(e, data) {
-                if (data.active) {
-                    ga('send', {
-                        hitType: 'event',
-                        eventCategory: category,
-                        eventAction: 'filter.' + data.group,
-                        eventLabel: data.label,
-                        eventValue: data.value
-                    });
-                }
+                var obj = {
+                    hitType: 'event',
+                    eventCategory: category,
+                    eventAction: (data.active ? 'select_' : 'deselect_') + data.group,
+                    eventLabel: data.label,
+                    eventValue: data.value
+                };
+                that.log('ga.send()', obj);
+                ga('send', obj);
             });
-    }
 
-    // GA is not loaded by requireJs, we need to check if it's available
-    var count = 0,
-        gaInterval = setInterval(function() {
-            if (typeof ga == "object") {
-                initHandlers();
-                clearInterval(gaInterval);
-                return;
-            }
-            count++;
-            if (count > 20) { // Stop check after 10 seconds
-                clearInterval(gaInterval);
-            }
-        }, 500);
+        return this;
+    };
+
+    return new Interact().init();
 });
