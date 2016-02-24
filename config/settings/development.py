57540aaadba06309d02f912833b43cc6c0124933
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2016-02-23 20:18:56 -0800

    update s3 references from pubstream to etalia

--- config/settings/development.py
+++ config/settings/development.py
@@ -39,9 +39,9 @@ STATICFILES_DIRS = (
 )
 
 # AWS S3 Buckets
-NLP_DATA_BUCKET_NAME = 'pubstream-development-nlp-data'
-NLP_MODELS_BUCKET_NAME = 'pubstream-development-nlp-models'
-NLP_MS_BUCKET_NAME = 'pubstream-development-nlp-ms'
+NLP_DATA_BUCKET_NAME = 'etalia-development-nlp-data'
+NLP_MODELS_BUCKET_NAME = 'etalia-development-nlp-models'
+NLP_MS_BUCKET_NAME = 'etalia-development-nlp-ms'
 AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
 
 
