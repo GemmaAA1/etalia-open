bb55ee3f5c2b7f2e1f08ede10fe357d92735c9cc
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2016-03-19 11:43:15 -0700

    debug deploy.py

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -61,14 +61,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("stack", help="(str) stack name to be deploy", type=str)
     parser.add_argument("-p", "--parallel", help="deploy in parallel", action="store_true")
-    parser.add_argument("-s", "--slack", help="Send deployment info to slack", action="store_true")
-    parser.add_argument("-a", "--assets", help="Compiled and push assests before deployment", action="store_true")
+    parser.add_argument("-s", "--slack", help="deploy in parallel", action="store_true")
+    parser.add_argument("-np", "--no-push", help="deploy in parallel", action="store_true",
+                        dest='no_push')
     args = parser.parse_args()
 
     if args.slack:
         send_deploy_version_message(args.stack)
 
-    if args.assets:
+    if not args.no_push:
         checkout_master_and_push_recompiled_assets()
 
     if args.parallel:
@@ -92,4 +93,5 @@ if __name__ == '__main__':
         # Go off maintenance
         call(['fab', 'set_hosts:{stack},apps,*'.format(stack=args.stack), 'go_off_maintenance'])
 
-    send_deploy_version_message(args.stack, done=True)
+    if args.slack:
+        send_deploy_version_message(args.stack, done=True)
