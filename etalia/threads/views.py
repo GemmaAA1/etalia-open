683aa2f451414dc269215c5b96e6c9fabc1a737f
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2016-04-01 00:52:02 -0700

    testing template nested dict

--- etalia/threads/views.py
+++ etalia/threads/views.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.views.generic import UpdateView, FormView, TemplateView, CreateView, \
-    DeleteView, ListView
-from braces.views import LoginRequiredMixin, UserPassesTestMixin
-from django.core.urlresolvers import reverse
-from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView, DetailView, ListView
+from braces.views import LoginRequiredMixin
 
 from etalia.core.mixins import AjaxableResponseMixin
-from .forms import ThreadCreateForm, ThreadUpdateForm, ThreadPostForm, \
-    ThreadPostCommentForm, ThreadUserForm
-from .models import Thread, ThreadPost, ThreadComment, ThreadUser
-from .api.serializers import ThreadSerializer, ThreadPostSerializer, \
-    ThreadCommentSerializer, ThreadUserSerializer
+from .models import Thread
+from .api.serializers import ThreadNestedSerializer
 
 
-class ThreadView(LoginRequiredMixin, AjaxableResponseMixin, TemplateView):
+class ThreadView(LoginRequiredMixin, AjaxableResponseMixin, DetailView):
 
+    model = Thread
     template_name = 'threads/thread.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ThreadView, self).get_context_data(**kwargs)
+        context['thread'] = ThreadNestedSerializer(
+            instance=self.get_object(),
+            context={'request': self.request}).data
+        return context
 
 thread = ThreadView.as_view()
 
 
-class MyThreadsView(LoginRequiredMixin, AjaxableResponseMixin, TemplateView):
+class MyThreadsView(LoginRequiredMixin, AjaxableResponseMixin, ListView):
 
     template_name = 'threads/my_threads.html'
 
