17ffc76fd0f433dac10e99200db4e76fbb655ffb
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2016-07-27 16:55:29 -0700

    update S3 storage with boto3

--- etalia/nlp/mixins.py
+++ etalia/nlp/mixins.py
@@ -2,13 +2,15 @@
 from __future__ import unicode_literals, absolute_import
 
 import os
-import boto
+import boto3
+import botocore
 import glob
 import tarfile
 import logging
 import lz4
-from boto.s3.key import Key
+import threading
 from progressbar import ProgressBar, Percentage, Bar, ETA
+from django.core.exceptions import ImproperlyConfigured
 
 
 class S3Mixin(object):
@@ -18,6 +20,25 @@ class S3Mixin(object):
     AWS_SECRET_ACCESS_KEY = ''
     PATH = ''
     name = ''
+
+    class ProgressPercentage(object):
+
+        def __init__(self, content_length=None):
+            self._size = content_length
+            self._seen_so_far = 0
+            self._pbar = None
+
+        def __call__(self, bytes_amount):
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            if self._pbar is None:
+                self._pbar = ProgressBar(widgets=[Percentage(), Bar(), ' ', ETA()],
+                                         maxval=100, term_width=75).start()
+            if not self._pbar.finished:
+                self._pbar.update(percentage)
+                if percentage == 100:
+                    # close progress bar
+                    self._pbar.finish()
 
     def s3callback(self, complete, tot):
         if not hasattr(self, 'pbar'):
@@ -32,109 +53,131 @@ class S3Mixin(object):
             self.pbar.finish()
             self.pbar = None
 
+    def connect_and_check_bucket(self):
+
+        bucket_name = self.BUCKET_NAME
+        s3 = boto3.resource(
+            's3',
+            aws_access_key_id=self.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY)
+
+        # Check existence of bucket
+        try:
+            s3.meta.client.head_bucket(Bucket=bucket_name)
+        except botocore.exceptions.ClientError as e:
+            # If a client error is thrown, then check that it was a 404 error.
+            # If it was a 404 error, then the bucket does not exist.
+            error_code = int(e.response['Error']['Code'])
+            if error_code == 404 or 403:
+                raise ImproperlyConfigured('bucket name ({}) does not exist'.format(bucket_name))
+
+        return s3
+
+    def get_key(self, compress=True):
+        # Build key
+        if compress:
+            return '{}.tar.lz4'.format(self.name)
+        else:
+            return '{}.tar'.format(self.name)
+
+    def get_keys(self):
+        return [self.get_key(compress=False), self.get_key()]
+
     def push_to_s3(self, compress=True):
         """Upload object to Amazon s3 bucket"""
-        try:
-            bucket_name = self.BUCKET_NAME
-            conn = boto.connect_s3(self.AWS_ACCESS_KEY_ID,
-                                   self.AWS_SECRET_ACCESS_KEY)
-            bucket = conn.get_bucket(bucket_name)
 
-            # Compressing
-            logging.info('Archiving {}...'.format(self.name))
-            tar_path = os.path.join(self.PATH, '{}.tar'.format(self.name))
-            lz4_path = os.path.join(self.PATH, '{}.tar.lz4'.format(self.name))
+        s3 = self.connect_and_check_bucket()
 
-            with tarfile.open(tar_path, 'w') as in_file:
-                for filename in glob.glob(os.path.join(self.PATH,
-                                                       '{stem}.*'.format(
-                                                           stem=self.name))):
-                    in_file.add(filename, arcname=os.path.split(filename)[1])
+        # Archive
+        logging.info('Archiving {}...'.format(self.name))
+        tar_path = os.path.join(self.PATH, '{}.tar'.format(self.name))
+        lz4_path = os.path.join(self.PATH, '{}.tar.lz4'.format(self.name))
 
-            if compress:
-                logging.info('{} LZ4 compressing...'.format(self.name))
-                with open(tar_path, 'rb') as in_file:
-                    with open(lz4_path, 'wb') as out_file:
-                        out_file.write(lz4.compress(in_file.read()))
-                key = '{}.tar.lz4'.format(self.name)
-                upload_path = lz4_path
-            else:
-                key = '{}.tar'.format(self.name)
-                upload_path = tar_path
+        with tarfile.open(tar_path, 'w') as in_file:
+            for filename in glob.glob(os.path.join(self.PATH,
+                                                   '{stem}.*'.format(
+                                                       stem=self.name))):
+                in_file.add(filename, arcname=os.path.split(filename)[1])
 
-            # Upload
-            logging.info('Uploading {} on s3...'.format(self.name))
-            # create a key to keep track of our file in the storage
-            k = Key(bucket)
-            k.key = key
-            k.set_contents_from_filename(upload_path, cb=self.s3callback, num_cb=100)
-            # remove tar and lz4 file
-            os.remove(tar_path)
-            if compress:
-                os.remove(lz4_path)
-            logging.info('Upload {} DONE'.format(self.name))
+        # Compress
+        if compress:
+            logging.info('{} LZ4 compressing...'.format(self.name))
+            with open(tar_path, 'rb') as in_file:
+                with open(lz4_path, 'wb') as out_file:
+                    out_file.write(lz4.compress(in_file.read()))
+            upload_path = lz4_path
+        else:
+            upload_path = tar_path
 
-        except Exception:
-            raise
+        # Get key
+        key = self.get_key(compress=compress)
+
+        # Upload
+        logging.info('Uploading {} on s3...'.format(self.name))
+        s3.meta.client.upload_file(
+            upload_path,
+            self.BUCKET_NAME,
+            key,
+            Callback=self.ProgressPercentage(float(os.path.getsize(upload_path))))
+
+        # Clean
+        os.remove(tar_path)
+        if compress:
+            os.remove(lz4_path)
+        logging.info('Upload {} DONE'.format(self.name))
 
     def pull_from_s3(self, compress=True):
         """Download object from Amazon s3 bucket"""
+
+        s3 = self.connect_and_check_bucket()
+
+        # Get key
+        key = self.get_key(compress=compress)
+
+        # Download
+        download_path = os.path.join(self.PATH, key)
+        logging.info('Downloading {0} from s3 to {1}...'.format(self.name,
+                                                          self.PATH))
+        content_length = s3.Object(self.BUCKET_NAME, key).content_length
+        s3.meta.client.download_file(
+            self.BUCKET_NAME,
+            key,
+            download_path,
+            Callback=self.ProgressPercentage(content_length))
+
+        # Decompress
+        logging.info('{} Decompressing...'.format(self.name))
+        tar_path = os.path.join(self.PATH, '{}.tar'.format(self.name))
+        if compress:
+            with open(download_path, 'rb') as in_file:
+                with open(tar_path, 'wb') as out_file:
+                    out_file.write(lz4.decompress(in_file.read()))
+
+        # Expand
+        with tarfile.open(tar_path, 'r') as in_file:
+                in_file.extractall(self.PATH)
+
+        # Clean
         try:
-            bucket_name = self.BUCKET_NAME
-            conn = boto.connect_s3(self.AWS_ACCESS_KEY_ID,
-                                   self.AWS_SECRET_ACCESS_KEY)
-            bucket = conn.get_bucket(bucket_name)
-            if compress:
-                key = '{}.tar.lz4'.format(self.name)
-            else:
-                key = '{}.tar'.format(self.name)
-            item = bucket.get_key(key)
-
-            # Downloading
-            download_path = os.path.join(self.PATH, key)
-            logging.info('Downloading {0} from s3 to {1}...'.format(self.name,
-                                                              self.PATH))
-            item.get_contents_to_filename(download_path,
-                                          cb=self.s3callback,
-                                          num_cb=100)
-
-            # Decompressing
-            logging.info('{} Decompressing...'.format(self.name))
-            tar_path = os.path.join(self.PATH, '{}.tar'.format(self.name))
-            if compress:
-                with open(download_path, 'rb') as in_file:
-                    with open(tar_path, 'wb') as out_file:
-                        out_file.write(lz4.decompress(in_file.read()))
-
-            # extract archive
-            with tarfile.open(tar_path, 'r') as in_file:
-                    in_file.extractall(self.PATH)
-            # remove tar file
-            try:
-                os.remove(download_path)
-            except FileNotFoundError:
-                pass
-            try:
-                os.remove(tar_path)
-            except FileNotFoundError:
-                pass
-            logging.info('{} Done'.format(self.name))
-        except Exception:
-            raise
+            os.remove(download_path)
+        except FileNotFoundError:
+            pass
+        try:
+            os.remove(tar_path)
+        except FileNotFoundError:
+            pass
+        logging.info('{} Done'.format(self.name))
 
     def delete_on_s3(self):
         """Delete object from Amazon s3 bucket"""
-        try:
-            bucket_name = self.BUCKET_NAME
-            conn = boto.connect_s3(self.AWS_ACCESS_KEY_ID,
-                                   self.AWS_SECRET_ACCESS_KEY)
-            bucket = conn.get_bucket(bucket_name)
-            keys = [self.name + '.tar.lz4', self.name + '.tar']
-            for key in keys:
-                item = bucket.get_key(key)
-                if item:
-                    logging.info('{} Deleting on s3...'.format(self.name))
-                    item.delete()
-                    logging.info('{} Done'.format(self.name))
-        except Exception:
-            raise
+
+        s3 = self.connect_and_check_bucket()
+        bucket = s3.Bucket(self.BUCKET_NAME)
+
+        # Get keys
+        keys = self.get_keys()
+
+        # Delete
+        for obj in bucket.objects.all():
+            if obj.key in keys:
+                obj.delete()
