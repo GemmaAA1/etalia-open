d8d5ec9bcb2b9adacf189ec9bd3fede104f972e3
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2016-08-01 10:49:36 -0700

    externalize elastic_ip config

---
+++ deploy/aws/elastic_ip.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+import environ
+env = environ.Env()
+
+ELASTIC_IP_MAPPING = {
+    'redis': {
+        'ip': env.str('REDIS_ELASTIC_IP'),
+        'allocation_id': env.str('REDIS_ELASTIC_ALLOCATION_ID')
+    },
+    'master': {
+        'ip': env.str('RABBITMQ_ELASTIC_IP'),
+        'allocation_id': env.str('RABBITMQ_ELASTIC_ALLOCATION_ID')
+    }
+}
