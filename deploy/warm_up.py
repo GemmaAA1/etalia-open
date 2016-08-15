fb35ac38aae481e14585aac4dac6d805586d1ce8
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2016-08-15 12:43:24 -0700

    fix warm enignes script

--- deploy/warm_up.py
+++ deploy/warm_up.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
-from kombu.common import Broadcast
 
 
 def warm_up():
-    warmup_paper_engine.delay('dummy')
-    warmup_thread_engine.delay('dummy')
-    warmup_nlp_model.delay('dummy')
+    # We are looping here to deal with worker concurrency.
+    # I.e warm up tasks are broadcasting to workers but
+    # within one worker if concurrency is >1, task is only seen by 1 thread
+    for _ in range(10):
+        warmup_paper_engine.delay()
+        warmup_thread_engine.delay()
+        warmup_nlp_model.delay()
 
 
 if __name__ == '__main__':
