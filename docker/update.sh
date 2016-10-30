cfe8e2ea3d45693a03f2988e8b62d2d7775a5d0d
Etienne Dauvergne <contact@ekyna.com>
2016-10-30 14:12:36 +0100

    Externalize and compress python docker image. Remove useless services from docker compose stack. Use built image for install and update scripts. Updated README.

--- docker/update.sh
+++ docker/update.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-# update etalia
-python setup/manager.py --update
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+OPTIONS="-i --rm \
+    --volumes-from etalia_web \
+    --network etalia-network \
+    --link etalia_db:db \
+    --link etalia_rabbit:rabbit \
+    --link etalia_redis:redis \
+    --env-file $DIR/.envs \
+    etalia/python-dev"
+
+docker run $OPTIONS python setup/manager.py --update
