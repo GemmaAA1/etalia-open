eaacc96b5326236819a3b09378f30599a467f22c
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2016-11-21 23:44:42 -0800

    update docker pycharm support

---
+++ docker/dev/build-dev.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+CUR_DIR="$(pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="$(dirname ""$(dirname "$DIR")"")"
+cd $DIR
+
+# Generate or re-use SSH keys. The ssh_keys directory should be in .gitignore
+mkdir -p ssh_keys
+if [ ! -f ssh_keys/myuser-sshd ] ; then
+    echo "Generating SSH key"
+    # Remove any associated public key also
+    rm -f ssh_keys/myuser-sshd*
+    ssh-keygen -t rsa -f ssh_keys/myuser-sshd -N ""
+else
+    echo "Using existing SSH key"
+fi
+
+# NB: This is only to use remote interpreter from pycharm within the docker container
+# unfortunately docker native doesn't mount in /Applications, either change that or do this
+rm -rf ~/.pycharm_helpers
+cp -R /Applications/PyCharm.app/Contents/helpers ~/.pycharm_helpers
+
+# Now build the sshd server image, which should be based on the main image.
+cd $ROOT_DIR
+docker build --force-rm=true -f docker/dev/Dockerfile-ssh --tag etalia/python-dev-ssh .
+
+cd $CUR_DIR
--- docker/dev/set_root_pw.sh
+++ docker/dev/set_root_pw.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+SSH_KEY="$(cat /myuser-sshd.pub)"
+
 if [ -z "${SSH_KEY}" ]; then
 	echo "=> Please pass your public key in the SSH_KEY environment variable"
 	exit 1
 fi
 
-for MYHOME in /root /home/docker; do
+for MYHOME in /root /home/myuser; do
 	echo "=> Adding SSH key to ${MYHOME}"
 	mkdir -p ${MYHOME}/.ssh
 	chmod go-rwx ${MYHOME}/.ssh
@@ -13,7 +15,7 @@ for MYHOME in /root /home/docker; do
 	chmod go-rw ${MYHOME}/.ssh/authorized_keys
 	echo "=> Done!"
 done
-chown -R docker:docker /home/docker/.ssh
+chown -R myuser:myuser /home/myuser/.ssh
 
 echo "========================================================================"
 echo "You can now connect to this container via SSH using:"
