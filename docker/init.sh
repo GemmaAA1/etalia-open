f156dbda3a9ae60bb53fbf801dbd786de25f81a8
Nicolas Pannetier <nicolas.pannetier@gmail.com>
2016-11-22 00:18:46 -0800

    add init shell script

---
+++ docker/init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+OPTIONS="-i --rm \
+    --volumes-from etalia_web \
+    --network etalia-network \
+    --link etalia_db:db \
+    --env-file $DIR/.envs \
+    etalia/python-dev"
+
+docker run $OPTIONS python setup/manager.py --init
